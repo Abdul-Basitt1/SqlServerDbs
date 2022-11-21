2e826e9ca19ace0412f01dcba664fa532f6ccb3f
Abdul-Basitt1 <abdulrocker4@gmail.com>
2022-11-21 23:39:38 +0500

    Add project files.

---
+++ SqlServerDbs/Customer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SqlServerDbs
+{
+    public class Customer
+    {
+
+        private static string mycon = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Phone { get; set; }
+        
+        private const string InsertQuery = "Insert Into customer(CompanyName,ContactName,Phone) Values(@CompanyName, @ContactName, @Phone)";
+        public bool InsertCustomer(Customer customer)
+        {
+            int rows;
+            using (SqlConnection con = new SqlConnection(mycon))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(InsertQuery, con))
+                {
+                    com.Parameters.AddWithValue("@CompanyName", customer.CompanyName);
+                    com.Parameters.AddWithValue("@ContactName", customer.ContactName);
+                    com.Parameters.AddWithValue("@Phone", customer.Phone);
+                    rows = com.ExecuteNonQuery();
+                }
+            }
+            return (rows > 0) ? true : false;
+        }
+
+        private const string UpdateQuery = "Update customer set CompanyName=@CompanyName,ContactName=@ContactName,Phone=@Phone where CustomerID=@CustomerID";
+        public bool UpdateCustomer(Customer customer)
+        {
+            int rows;
+            using (SqlConnection con = new SqlConnection(mycon))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(UpdateQuery, con))
+                {
+                    com.Parameters.AddWithValue("@CustomerID", customer.CustomerID);
+                    com.Parameters.AddWithValue("@CompanyName", customer.CompanyName);
+                    com.Parameters.AddWithValue("@ContactName", customer.ContactName);
+                    com.Parameters.AddWithValue("@Phone", customer.Phone);
+                    rows = com.ExecuteNonQuery();
+                }
+            }
+            return (rows > 0) ? true : false;
+        }
+
+        private const string DeleteQuery = "Delete from customer where CustomerID=@CustomerID";
+        public bool DeleteCustomer(Customer customer)
+        {
+            int rows;
+            using (SqlConnection con = new SqlConnection(mycon))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(DeleteQuery, con))
+                {
+                    com.Parameters.AddWithValue("@CustomerID", customer.CustomerID);
+                    com.Parameters.AddWithValue("@CompanyName", customer.CompanyName);
+                    com.Parameters.AddWithValue("@ContactName", customer.ContactName);
+                    com.Parameters.AddWithValue("@Phone", customer.Phone);
+                    rows = com.ExecuteNonQuery();
+                }
+            }
+            return (rows > 0) ? true : false;
+        }
+
+        private const string SelectQuery = "Select * from customer";
+        public DataTable GetEmployees()
+        {
+            var datatable = new DataTable();
+            using (SqlConnection con = new SqlConnection(mycon))
+            {
+                con.Open();
+                using (SqlCommand com = new SqlCommand(SelectQuery, con))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(com))
+                    {
+                        adapter.Fill(datatable);
+                    }
+                }
+            }
+            return datatable;
+
+        }
+    }
+}
---
+++ SqlServerDbs/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlServerDbs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Customer customer = new Customer();
+
+        private void addrecord(object sender, EventArgs e)
+        {
+            customer.CompanyName = textBox1.Text;
+            customer.ContactName = textBox2.Text;
+            customer.Phone = textBox3.Text;
+            var success = customer.InsertCustomer(customer);
+            dataGridView1.Visible = true;
+            dataGridView1.DataSource = customer.GetEmployees();
+            if (success)
+            {
+                ClearControls();
+                MessageBox.Show("Customer Added Succesfully");
+            }
+            else
+            {
+                MessageBox.Show("Customer Addtion Failed");
+            }
+        }
+
+        private void clearfields(object sender, EventArgs e)
+        {
+            ClearControls();
+        }
+
+        private void ClearControls()
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            textBox4.Text = null;
+            textBox5.Text = null;
+        }
+
+        private void delete_cust(object sender, EventArgs e)
+        {
+            customer.CustomerID = textBox4.Text;
+            var success = customer.DeleteCustomer(customer);
+            dataGridView1.Visible = true;
+            dataGridView1.DataSource = customer.GetEmployees();
+            if (success)
+            {
+                ClearControls();
+                MessageBox.Show("Customer Deleted Succesfully");
+            }
+            else
+            {
+                MessageBox.Show("Customer Deletion Failed");
+            }
+        }
+
+        private void update_cust(object sender, EventArgs e)
+        {
+            customer.CompanyName = textBox1.Text;
+            customer.ContactName = textBox2.Text;
+            customer.Phone = textBox3.Text;
+            customer.CustomerID = textBox5.Text;
+            dataGridView1.Visible = true;
+            var success = customer.UpdateCustomer(customer);
+            dataGridView1.DataSource = customer.GetEmployees();
+            if (success)
+            {
+                ClearControls();
+                MessageBox.Show("Customer Updated Succesfully");
+            }
+            else
+            {
+                MessageBox.Show("Customer Updation Failed");
+            }
+
+        }
+
+        private void fillselect_items_tofields(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            dataGridView1.Visible = true;
+            var index = e.RowIndex;
+            textBox1.Text = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[index].Cells[3].Value.ToString();
+        }
+    }
+}
